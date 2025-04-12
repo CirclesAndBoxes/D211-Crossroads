d59f3f80d55bedc60dacb9026a4b1ffc272b94b0
Arjun Shah <shah1589@students.d211.org>
2025-04-12 13:03:13 -0500

    Stash
    hkhjhjhjk

---
+++ Assets/Main_menu_script.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+
+    }
+    
+    public void openOptions()
+    {
+        Debug.Log("Hi");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+}
